e879f1aabbc5442e4d948adf854dd5ddfff677f5
maxelonej <maxelone@yandex.ru>
2024-11-14 23:34:18 +0400

    Remove comments

--- random_simple_lithorrhea.php
+++ random_simple_lithorrhea.php
@@ -1,24 +1,16 @@
 <?php
 
-// Создание мапы для шифра
 function createMap(): array
 {
   $consonants = ['б', 'в', 'г', 'д', 'ж', 'з', 'к', 'л', 'м', 'н'];
   $mappedConsonants = ['щ', 'ш', 'ч', 'ц', 'х', 'ф', 'т', 'с', 'р', 'п'];
 
-  // Мапа ("согласная":"зашифрованная согласная согласно простой литореи")
   $map = array_combine($consonants, $mappedConsonants);
-  $keys = array_keys($map); // Согласные
-  $values = array_values($map); // Зашифрованные согласные
-  // Перемешивание зашифрованных согласных, вместо простой литореи - рандом
+  $keys = array_keys($map);
+  $values = array_values($map);
   shuffle($values);
-  // Мапа ("согласная":"рандом зашифрованная согласная")
   $map = array_combine($keys, $values);
 
-  /*
-    Добавление в мапу согласных и к ним
-    зашифрованных согласных в виде верхнего регистра
-  */
   $upperMap = array_combine(
     array_map('mb_strtoupper', $keys),
     array_map('mb_strtoupper', $values)
@@ -27,60 +19,36 @@ function createMap(): array
   return array_merge($map, $upperMap);
 }
 
-// Шифрование
 function encrypt($originalText, $map): string
 {
-  /*
-  echo "Исходный текст: $text\n";
-  echo "Карта шифрования: ";
-  print_r($map);
-  */
-
   $encryptedText = $originalText;
 
-  // Проход по карте шифрования и замена символов
   foreach ($map as $consonant => $mappedConsonant) {
-    // Генерирование уникального символа
     $tempSymbol = uniqid();
-    // Замена согласной на уникальный символ
     $encryptedText = str_replace($consonant, $tempSymbol, $encryptedText);
-    // Замена зашифрованной согласной на уникальный символ
     $encryptedText = str_replace($mappedConsonant, $consonant, $encryptedText);
-    // Шифрование согласной
     $encryptedText = str_replace($tempSymbol, $mappedConsonant, $encryptedText);
   }
 
   return $encryptedText;
 }
 
-// Дешифрование
 function decrypt($encryptedText, $map): string
 {
   $reverseMapping = array_flip($map);
-  /*
-  echo "Зашифрованный текст: $encryptedText\n";
-  echo "Карта дешифрования: ";
-  print_r($reverseMapping);
-  */
 
   $decryptedText = $encryptedText;
 
-  // Проход по карте дешифрования и замена символов
   foreach ($reverseMapping as $mappedConsonant => $consonant) {
-    // Генерирование уникального символа
     $tempSymbol = uniqid();
-    // Замена зашифрованной согласной на уникальный символ
     $decryptedText = str_replace($mappedConsonant, $tempSymbol, $decryptedText);
-    // Замена согласной на уникальный символ
     $decryptedText = str_replace($consonant, $mappedConsonant, $decryptedText);
-    // Расшифрование
     $decryptedText = str_replace($tempSymbol, $consonant, $decryptedText);
   }
 
   return $decryptedText;
 }
 
-// Тестирование шифрования, дешифрования
 function testEncryptionDecryption($originalText)
 {
   $encryptionMap = createMap();
@@ -93,7 +61,6 @@ function testEncryptionDecryption($originalText)
   printf("Зашифрованный текст: %s\n", $encryptedText);
   printf("Дешифрованный текст: %s\n", $decryptedText);
 
-  // Оригинальный текст совпадает с дешифром, тогда/иначе
   if ($originalText === $decryptedText) {
     echo "Тест: [✅ пройден]; Дешифрованный текст совпадает с исходным.\n";
   } else {
@@ -102,6 +69,6 @@ function testEncryptionDecryption($originalText)
   echo "-------------------------------\n";
 }
 
-$animal = "Крокодил"; // Пример текста для шифра
-testEncryptionDecryption($animal); // Тестирование шифра, дешифра
+$animal = "Крокодил";
+testEncryptionDecryption($animal);
 testEncryptionDecryption("Привет, Мир!");
