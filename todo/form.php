47bbebed8be93f708380565268a754e00ad3bc27
maxelonej <maxelone@yandex.ru>
2024-11-28 21:36:44 +0400

    feat: Add task, remove task

--- todo/form.php
+++ todo/form.php
@@ -1,5 +1,36 @@
 <?php
+session_start();
 
-require_once("validation.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $task = $_POST['task'] ?? '';
+  require_once("validation.php");
+  // validation.php ($task)
+  $validTask = trim($task);
+
+  if ($validTask) {
+    if (!isset($_SESSION['tasks'])) {
+      $_SESSION['tasks'] = [];
+    }
+
+    $_SESSION['tasks'][] = [
+      'name' => $validTask
+    ];
+
+    header("Location: index.php");
+    exit;
+  } else {
+    // valid error msg
+  }
+}
+
+if (isset($_POST['delete_task_id'])) {
+  $taskId = (int) $_POST['delete_task_id'];
+  if (isset($_SESSION['tasks'][$taskId])) {
+    unset($_SESSION['tasks'][$taskId]);
+    $_SESSION['tasks'] = array_values($_SESSION['tasks']);
+    echo json_encode(['success' => true]);
+    exit;
+  }
+}
 
 ?>
